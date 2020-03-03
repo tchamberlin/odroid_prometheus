204ff88693436777a643e16da0ca9c2e1c8a4611
Thomas Chamberlin <tchamber@nrao.edu>
2020-03-03 15:31:29 -0500

    Added README; cleaned up a bunch of code

--- export_cpu_temp.py
+++ export_cpu_temp.py
@@ -1,33 +1,45 @@
-import prometheus_client as prom
+"""Prometheus Exporter for ODROID N2's two thermal zones"""
+
+
 import time
+
+import prometheus_client as prom
 
 
 def parse_temperature(path, decimal_places=3):
+    """Parse temperature from temperature file
+
+    Value will be something like '30123', which is actually 30.123"""
     with open(path) as file:
         contents = file.read()
+        # Split at where the decimal should be...
         num, decimal = (
             contents[: -(decimal_places + 1)],
             contents[-(decimal_places + 1) :],
         )
+        # ...then stick it in there and convert to float
         return float(f"{num}.{decimal}")
 
 
-if __name__ == "__main__":
-
-    thermal_zone_0_celcius_gauge = prom.Gauge(
-        "thermal_zone_0_celcius", "Thermal zone monitor (celcius)"
+def main():
+    thermal_zone_0_celsius_gauge = prom.Gauge(
+        "thermal_zone_0_celsius", "Thermal zone monitor (celsius)"
     )
-    thermal_zone_1_celcius_gauge = prom.Gauge(
-        "thermal_zone_1_celcius", "Thermal zone monitor (celcius)"
+    thermal_zone_1_celsius_gauge = prom.Gauge(
+        "thermal_zone_1_celsius", "Thermal zone monitor (celsius)"
     )
     prom.start_http_server(9190)
 
     while True:
-        thermal_zone_0_celcius_gauge.set(
+        thermal_zone_0_celsius_gauge.set(
             parse_temperature("/sys/devices/virtual/thermal/thermal_zone0/temp")
         )
-        thermal_zone_1_celcius_gauge.set(
+        thermal_zone_1_celsius_gauge.set(
             parse_temperature("/sys/devices/virtual/thermal/thermal_zone1/temp")
         )
 
         time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
